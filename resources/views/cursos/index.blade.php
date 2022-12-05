ad6816a34e2a40f1ab011ab31092d5cafe45e1d7
carlosfabianarevalo <cfarevalo3@misena.edu.co>
2022-12-05 08:25:03 -0500

    contador clicks

---
+++ resources/views/cursos/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.main')
+
+@section('titulo', 'Cursos')
+
+@section('content')
+    @livewire('curso')
+
+@endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,19 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <title>Grupos de estudiantes</title>
+    @livewireStyles
+    <title>@yield('titulo')</title>
 </head>
-<body class="">
-    <main class="vh-100">
-        <h1 class="text-center mt-5">Grupos de estudiantes</h1>
-        <div class="container mt-5 d-flex justify-content-evenly">
-            <a href="{{ route('cursos.index') }}" class="btn btn-lg btn-primary">Cursos</a>
-            <a href="" class="btn btn-lg btn-primary">Generar grupos</a>
+<body>
 
-        </div>
+    <main>
+        @yield('content')
     </main>
-
-
+    
+    @livewireScripts
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
